a33578ddf421ac2dec5e3ea6d6aaf7407431c46d
Geoffrey Vedernikoff <yefim323@gmail.com>
2013-12-17 03:11:51 -0500

    Added markdown parsing

--- Main.hs
+++ Main.hs
@@ -2,13 +2,23 @@
 -- by Lewis Ellis ellis and Geoffrey Vedernikoff veg
 
 {-# OPTIONS -Wall -fwarn-tabs -fno-warn-type-defaults #-}
-{-# LANGUAGE KindSignatures, ScopedTypeVariables, FlexibleInstances #-}
+{-# LANGUAGE ScopedTypeVariables, FlexibleInstances #-}
 
 module Main where
 import Prelude
 import System.Environment
+import Data.List.Split
+import Text.Pandoc
+
+markdownToHtml :: String -> String
+markdownToHtml = writeHtmlString def . readMarkdown def
 
 main :: IO ()
 main = do
-  [file] <- getArgs  
-  putStrLn file
+  [filename] <- getArgs
+  contents <- readFile filename
+  let slides = splitOn "\n--\n\n" contents
+  let html = map markdownToHtml slides -- might only want to do (tail slides)
+  _ <- mapM (\s -> putStrLn $ "line: " ++ s) html -- prints for debugging
+  -- writeFile outputFilename html
+  putStrLn "Heaver is done."
