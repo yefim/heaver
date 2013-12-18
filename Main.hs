17cfc0ffb88ef49fc1a950fb42d76569c93485ea
Geoffrey Vedernikoff <yefim323@gmail.com>
2013-12-17 19:08:01 -0500

    Hastache templates

--- Main.hs
+++ Main.hs
@@ -2,7 +2,7 @@
 -- by Lewis Ellis ellis and Geoffrey Vedernikoff veg
 
 {-# OPTIONS -Wall -fwarn-tabs -fno-warn-type-defaults #-}
-{-# LANGUAGE ScopedTypeVariables, FlexibleInstances #-}
+{-# LANGUAGE ScopedTypeVariables, FlexibleInstances, DeriveDataTypeable #-}
 
 module Main where
 import Prelude
@@ -10,21 +10,31 @@ import System.Environment
 import Data.List.Split
 import Text.Pandoc
 import Data.Yaml.YamlLight
-import Data.ByteString.Char8 (pack)
-import Data.Maybe
+-- import Data.ByteString.Char8 (pack)
+-- import Data.Maybe
+import qualified Data.ByteString.Lazy as LZ
+import Data.Data 
+import Text.Hastache
+import Text.Hastache.Context
 
 markdownToHtml :: String -> String
 markdownToHtml = writeHtmlString def . readMarkdown def
+
+template :: String
+template = "{{#slides}}\n{{{ . }}}\n{{/slides}}"
+
+data Slides = Slides { slides :: [String] } deriving (Data, Typeable)
 
 main :: IO ()
 main = do
   [filename] <- getArgs
   contents <- readFile filename
-  let (yaml:slides) = splitOn "\n--\n" contents
-  let html = map markdownToHtml slides
-  metadata <- parseYaml yaml
-  -- let author = fromJust $ lookupYL (YStr $ pack "author") metadata
-  -- let name = fromJust $ lookupYL (YStr $ pack "name") author
-  -- _ <- mapM (\s -> putStrLn $ "line: " ++ s) html -- prints for debugging
-  -- writeFile outputFilename html
+  let (y:s) = splitOn "\n--\n" contents
+  let html = map markdownToHtml s
+  metadata <- parseYaml y
+
+  let context = mkGenericContext $ Slides html
+  o <- hastacheStr defaultConfig (encodeStr template) context
+  LZ.writeFile "out.html" o
+
   putStrLn "Heaver is done."
